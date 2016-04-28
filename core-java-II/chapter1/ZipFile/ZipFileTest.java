ee07429623cef1c1a61a43b03a405515e3ea0d69
zh3n <zhen.li2@ge.com>
2016-04-28 18:18:47 +0800

    Add zip File read

--- core-java-II/chapter1/ZipFile/ZipFileTest.java
+++ core-java-II/chapter1/ZipFile/ZipFileTest.java
@@ -1,4 +1,4 @@
-package chapter1.File;
+package chapter1.ZipFile;
 
 import java.io.IOException;
 import java.nio.file.*;
@@ -7,12 +7,21 @@ import java.nio.file.attribute.BasicFileAttributes;
 /**
  * Created by 502396513 on 4/25/2016.
  */
-public class FileTest {
+public class ZipFileTest {
     public static void main(String[] args) throws IOException {
         Path dir = Paths.get("C:/Users/502396513/Downloads");
-        try (DirectoryStream<Path> entities = Files.newDirectoryStream(dir, "**.msi")) {
+        try (DirectoryStream<Path> entities = Files.newDirectoryStream(dir, "Chart.zip")) {
             for (Path entry : entities) {
                 System.out.println(entry.getFileName());
+                System.out.println(entry.toString());
+                FileSystem fs = FileSystems.newFileSystem(entry, null);
+//                Files.copy(fs.getPath("Chart.js"), Paths.get("C:/Users/502396513/Downloads/111/test"));
+                Files.walkFileTree(fs.getPath("/"), new SimpleFileVisitor<Path>() {
+                    public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) {
+                        System.out.println(file);
+                        return FileVisitResult.CONTINUE;
+                    }
+                });
             }
         } catch (IOException e) {
             e.printStackTrace();
