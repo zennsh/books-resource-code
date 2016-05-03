492e5f039770cbf71874b90b56410bfcb273093e
zh3n <zhen.li2@ge.com>
2016-05-03 10:48:51 +0800

    Add Regularization Demo

---
+++ core-java-II/chapter1/Regex/RegexTest.java
@@ -0,0 +1,17 @@
+package chapter1.Regex;
+
+import java.io.BufferedInputStream;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.nio.ByteBuffer;
+import java.nio.CharBuffer;
+import java.nio.file.Paths;
+
+/**
+ * Created by ZHEN on 4/29/2016.
+ */
+public class RegexTest {
+    public static void main(String[] args) throws FileNotFoundException {
+//        CharBuffer buffer = new BufferedInputStream(new FileInputStream("/resource/test.txt"));
+    }
+}
